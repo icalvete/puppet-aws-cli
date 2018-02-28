237ee83dcf71a4bb86aa10e86786fd02256d72c5
Israel Calvete <icalvete@gmail.com>
2018-02-28 10:29:10 +0100

    littlebugs.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,6 @@
 class aws_cli::config {
 
-  aws_cli::configure{"aws_cli_configure_${aws::user}":
+  aws_cli::configure{"aws_cli_configure_${aws_cli::user}":
     user                  => $aws_cli::user,
     home                  => $aws_cli::final_home,
     aws_access_key_id     => $aws_cli::aws_access_key_id,
